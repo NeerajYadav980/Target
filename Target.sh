f63d0c1bb72a611cc3533b3d9e6bc69ecac74c89
sutharsan sparkz <62829664+Sparkz-technology@users.noreply.github.com>
2020-04-17 16:00:13 +0530

    Update Target.sh

--- Target.sh
+++ Target.sh
@@ -370,11 +370,12 @@ fi
 sleep 1
 done 
 
-}
+
 
 echo -e "\e[1;32m"
 toilet -f mono12  Target | lolcat
 
+}
 echo -e "\e[1;34m Created By @sparkz_technology \e[1;31m              v6.sparkz"
 dependencies
 start1
